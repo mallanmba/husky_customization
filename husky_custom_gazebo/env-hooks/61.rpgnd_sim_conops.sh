758f8f5091ca20d9016fabf421c249eb95a9b12b
mallanmba <mallanmba@gmail.com>
2016-08-18 12:46:29 -0700

    move moon mesh under models to follow gazebo convention

--- husky_custom_gazebo/env-hooks/61.rpgnd_sim_conops.sh
+++ husky_custom_gazebo/env-hooks/61.rpgnd_sim_conops.sh
@@ -2,4 +2,5 @@
 ## there has got to be a better way than setting environment variables to do this
 ## FIXME: do this without setting environment variables!
 #export GAZEBO_RESOURCE_PATH="$(catkin_find --first-only husky_custom_gazebo meshes):${GAZEBO_RESOURCE_PATH}"
-export GAZEBO_MODEL_PATH="$(catkin_find --first-only husky_custom_gazebo meshes):${GAZEBO_RESOURCE_PATH}"
+export GAZEBO_MODEL_PATH="$(catkin_find --first-only husky_custom_gazebo models):${GAZEBO_MODEL_PATH}"
+
--- misc/update-rpgnd_sim.sh
+++ misc/update-rpgnd_sim.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-wget 
+FILE=rpgnd_sim_ws.rosinstall
+URL=https://github.com/mallanmba/husky_customization/raw/rpgnd_sim/indigo/misc/${FILE}
+
+echo "check ${FILE} is available..."
+wget -q "$URL"
+if [ $? -ne 0 ]; then
+  echo "ERROR: wget failed to query ${URL}"
+  exit
+fi
+
+echo "Ok, we can see remote ${FILE}."
+
+rm -f ${FILE}
+wget "$URL" &>/dev/null
+ln -sf ${FILE} .rosinstall
+
+echo "=================================="
+echo "== run wstool..."
+echo "=================================="
+
+wstool up
