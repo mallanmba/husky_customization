05c7ba57698176d298fbb8a1f28c72424b8cd0b3
mallanmba <mallanmba@gmail.com>
2016-09-09 11:00:14 -0700

    misc tweaks from session with OSRF

--- husky_custom_gazebo/env-hooks/61.rpgnd_sim_conops.sh
+++ husky_custom_gazebo/env-hooks/61.rpgnd_sim_conops.sh
@@ -1,6 +1,7 @@
 ## mallan: This adds our meshes directory to Gazebo's search path, but
 ## there has got to be a better way than setting environment variables to do this
 ## FIXME: do this without setting environment variables!
-#export GAZEBO_RESOURCE_PATH="$(catkin_find --first-only husky_custom_gazebo meshes):${GAZEBO_RESOURCE_PATH}"
 export GAZEBO_MODEL_PATH="$(catkin_find --first-only husky_custom_gazebo models):${GAZEBO_MODEL_PATH}"
+
+export GAZEBO_RESOURCE_PATH="$(catkin_find --first-only husky_custom_gazebo models):${GAZEBO_RESOURCE_PATH}"
 
